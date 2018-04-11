e744d98598b16cd961e3d57006b379ea856c0bc9
Michael Hansen <mihansen@microsoft.com>
2018-04-11 14:15:40 -0400

    Initial Commit

---
+++ go_daddy_dns_authenticator.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+#  ENVIRONMENT VARIABLES:
+#  godaddykey="KEY-VALUE"
+#  godaddysecret="SECRET-VALUE"
+
+# Strip only the top domain to get the zone id
+DOMAIN=$(echo -n $CERTBOT_DOMAIN | rev | cut -d"." -f1,2 | rev)
+SITE=$(expr match "$CERTBOT_DOMAIN" '\*\?\.\?\(.*\)\..*\..*')
+
+if [ -n "$SITE" ]; then
+   CREATESITE="_acme-challenge.${SITE}"
+else
+   CREATESITE="_acme-challenge"
+fi
+
+OUT=$(curl -s -X PUT https://api.godaddy.com/v1/domains/${DOMAIN}/records/TXT/${CREATESITE} \
+           -H "Authorization: sso-key ${godaddykey}:${godaddysecret}" \
+           -H "Content-Type: application/json" \
+           -H "Accept: application/json" \
+           --data '{"data": "'"$CERTBOT_VALIDATION"'", "ttl":3600}')
+
+sleep 25
